d33a39cfdc7c369e9aeeb056bd305bfdd0e64ba7
Breno Luiz <brenooh7@gmail.com>
2026-06-20 21:27:32 -0300

    corrigindo icones da pagina de admin

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import '@phosphor-icons/web/regular'
+import '@phosphor-icons/web/fill'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
